320a44ed6a1712914611efb2ffed778e9caa60c5
cooldown <fhgsrgtegs@gmail.com>
2025-02-25 18:35:30 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,5 +3,5 @@ if not isfolder("MeowConfigs") then
     makefolder("MeowConfigs")
 end
 
--- Load UI
-local UI = loadstring(game:HttpGet('https://raw.githubusercontent.com/cooldownn2/meow/main/meow/UI.lua'))()
+-- Load UI (keep your existing UI code untouched)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/cooldownn2/meow/main/meow/UI.lua'))()
